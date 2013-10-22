7bc0ce0757456bd4e262f7647f26ff2c9159e3f4
Ted Dunning <tdunning@apache.org>
2013-10-22 16:49:21 -0700

    First solid version of adaptive histogramming code

--- src/main/java/org/apache/mahout/math/stats/Histo.java
+++ src/main/java/org/apache/mahout/math/stats/Histo.java
@@ -19,6 +19,8 @@ package org.apache.mahout.math.stats;
 
 import com.google.common.collect.Maps;
 
+import java.util.Collection;
+import java.util.Iterator;
 import java.util.SortedMap;
 
 /**
@@ -29,15 +31,28 @@ public class Histo {
     private SortedMap<Double, Group> summary = Maps.newTreeMap();
     private int count = 0;
 
+    /**
+     * A histogram structure that will record a sketch of a distribution.
+     *
+     * @param compression How should accuracy be traded for size?  A value of N here will give quantile errors
+     *                    almost always less than 3/N with considerably smaller errors expected for extreme
+     *                    quantiles.  Conversely, you should expect to track about 5 N centroids for this
+     *                    accuracy.
+     */
     public Histo(double compression) {
         this.compression = compression;
     }
 
+    /**
+     * Adds a sample to a histogram.
+     * @param x  The value to add.
+     */
     public void add(double x) {
         SortedMap<Double, Group> before = summary.headMap(x);
         SortedMap<Double, Group> after = summary.tailMap(x);
 
         Group closest;
+        double q;
         if (before.size() == 0) {
             if (after.size() == 0) {
                 summary.put(x, new Group(x));
@@ -45,10 +60,12 @@ public class Histo {
                 return;
             } else {
                 closest = after.get(after.firstKey());
+                q = 0;
             }
         } else {
             if (after.size() == 0) {
                 closest = before.get(before.firstKey());
+                q = 1;
             } else {
                 double beforeGap = x - before.lastKey();
                 double afterGap = after.firstKey() - x;
@@ -57,9 +74,17 @@ public class Histo {
                 } else {
                     closest = after.get(after.firstKey());
                 }
+                // this tells us how many buckets are earlier
+                q = (double) before.size() / summary.size();
+                // and this converts to a true quantile assuming that the buckets have the correct distribution
+                // The tricky bit is that we then use this to enforce the correct distribution
+                q = q * q * (3 - 2 * q);
             }
         }
-        if (closest.count > count / compression) {
+        // the use of q here makes the threshold for addition small at each end.  This
+        // helps keep the relative error very small at the cost of increasing the number
+        // of centroids by a small integer factor
+        if (closest.count > count * 4 * q * (1 - q) / compression) {
             summary.put(x, new Group(x));
             count++;
         } else {
@@ -78,20 +103,77 @@ public class Histo {
         return summary;
     }
 
+    public double cdf(double x) {
+        Collection<Group> values = summary.values();
+        if (values.size() == 0) {
+            return Double.NaN;
+        } else if (values.size() == 1) {
+            return x < values.iterator().next().mean() ? 0 : 1;
+        } else {
+            double r = 0;
+
+            // we scan a across the centroids
+            Iterator<Group> it = values.iterator();
+            Group a = it.next();
+
+            // b is the look-ahead to the next centroid
+            Group b = it.next();
+
+            // initially, we set left width equal to right width
+            double left = (b.mean() - a.mean()) / 2;
+            double right = left;
+
+            // scan to next to last element
+            while (it.hasNext()) {
+                if (x < a.mean() + right) {
+                    return (r + a.size() * interpolate(x, a.mean() - left, a.mean() + right)) / count;
+                }
+                r += a.size();
+
+                a = b;
+                b = it.next();
+
+                left = right;
+                right = (b.mean() - a.mean()) / 2;
+            }
+
+            // for the last element, assume right width is same as left
+            left = right;
+            a = b;
+            if (x < a.mean() + right) {
+                return (r + a.size() * interpolate(x, a.mean() - left, a.mean() + right)) / count;
+            } else {
+                return 1;
+            }
+        }
+    }
+
+    private double interpolate(double x, double x0, double x1) {
+        return (x - x0) / (x1 - x0);
+    }
+
+
     public static class Group implements Comparable<Group> {
-        private double min, max, centroid;
+        private double centroid;
         private int count;
 
         public Group(double x) {
-            min = max = centroid = x;
+            centroid = x;
             count = 1;
         }
 
         public void add(double x) {
-            min = Math.min(x, min);
-            max = Math.max(x, max);
             count++;
-            centroid += (x - centroid) / count;
+            double delta = x - centroid;
+            centroid += delta / count;
+        }
+
+        public double mean() {
+            return centroid;
+        }
+
+        public int size() {
+            return count;
         }
 
         @Override
@@ -106,19 +188,9 @@ public class Histo {
         public int compareTo(Group o) {
             int r = Double.compare(centroid, o.centroid);
             if (r == 0) {
-                r = Double.compare(min, o.min);
-                if (r == 0) {
-                    r = Double.compare(max, o.max);
-                    if (r == 0) {
-                        r = count - o.count;
-                    }
-                }
+                r = count - o.count;
             }
             return r;
-        }
-
-        public int size() {
-            return count;
         }
     }
 }
--- src/test/java/org/apache/mahout/math/stats/HistoTest.java
+++ src/test/java/org/apache/mahout/math/stats/HistoTest.java
@@ -18,6 +18,7 @@
 package org.apache.mahout.math.stats;
 
 import org.apache.mahout.common.RandomUtils;
+import org.apache.mahout.common.RandomWrapper;
 import org.apache.mahout.math.jet.random.AbstractContinousDistribution;
 import org.apache.mahout.math.jet.random.Gamma;
 import org.apache.mahout.math.jet.random.Uniform;
@@ -25,50 +26,62 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-
 public class HistoTest {
     @Test
     public void testUniform() {
-        runTest(new Uniform(0, 1, RandomUtils.getRandom()), 1000);
+        RandomWrapper gen = RandomUtils.getRandom();
+        for (int i = 0; i < 20; i++) {
+            runTest(new Uniform(0, 1, gen), 1000,
+                    new double[]{0.0001, 0.001, 0.01, 0.1, 0.5, 0.9, 0.99, 0.999, 0.9999},
+                    new double[]{0.0001, 0.001, 0.01, 0.1, 0.5, 0.9, 0.99, 0.999, 0.9999},
+                    "uniform");
+        }
     }
 
     @Test
     public void testGamma() {
-        // this Gamma distribution is very heavily skewed so that the deciles are
-        // 0.0, 2.59e-26, 8.79e-12, 7.20e-09, 4.02e-07, 6.90e-06, 6.31e-05, 0.000402, 0.00190, 0.00741, 0.0273, Inf
-        // this severe skew means that we have to have positional accuracy that varies by nearly 30 orders of
-        // magnitude.
-        runTest(new Gamma(.1, 10, RandomUtils.getRandom()), 1000);
+        // this Gamma distribution is very heavily skewed.  The 0.1%-ile is 6.07e-30 while
+        // the median is 0.006 and the 99.9th %-ile is 33.6 while the mean is 1.
+        // this severe skew means that we have to have positional accuracy that
+        // varies by over 11 orders of magnitude.
+        RandomWrapper gen = RandomUtils.getRandom();
+        for (int i = 0; i < 20; i++) {
+            runTest(new Gamma(0.1, 0.1, gen), 1000,
+                    new double[]{6.0730483624079e-30, 6.0730483624079e-20, 6.0730483627432e-10, 5.9339110446023e-03,
+                            2.6615455373884e+00, 1.5884778179295e+01, 3.3636770117188e+01},
+                    new double[]{0.001, 0.01, 0.1, 0.5, 0.9, 0.99, 0.999},
+                    "gamma");
+        }
     }
 
     /**
      * Builds estimates of the CDF of a bunch of data points and checks that the centroids are accurately
      * positioned.  Accuracy is assessed in terms of the estimated CDF which is much more stringent than
      * checking position of quantiles with a single value for desired accuracy.
-     * @param gen          Random number generator that generates desired values.
-     * @param sizeGuide    Control for size of the histogram.
+     *
+     * @param gen       Random number generator that generates desired values.
+     * @param sizeGuide Control for size of the histogram.
+     * @param tag       Label for the output lines
      */
-    private void runTest(AbstractContinousDistribution gen, double sizeGuide) {
+    private void runTest(AbstractContinousDistribution gen, double sizeGuide, double[] xValues, double[] qValues, String tag) {
         Histo dist = new Histo(sizeGuide);
+        long t0 = System.nanoTime();
         for (int i = 0; i < 100000; i++) {
             dist.add(gen.nextDouble());
         }
-        Assert.assertTrue(dist.centroids().size() < 3 * sizeGuide);
-        int n = 0;
-        double q = 0;
-        for (Double x : dist.centroids().keySet()) {
-            Histo.Group c = dist.centroids().get(x);
-            double estimatedCdf = ((double) n + c.size() / 2) / dist.size();
-            double actualCdf = gen.cdf(x);
-            if (estimatedCdf > q) {
-                System.out.printf("%.3f\t%.3g\n", actualCdf, x);
-                q += 0.1;
-            }
-//            System.out.printf("%.4f\t%.4f\n", actualCdf, (estimatedCdf - actualCdf));
-            Assert.assertEquals(0, estimatedCdf - actualCdf, 1.0/Math.sqrt(sizeGuide));
-            n += c.size();
+        System.out.printf("# %fus per point\n", (System.nanoTime() - t0) * 1e-3 / 100000);
+        System.out.printf("# %d centroids\n", dist.centroids().size());
+
+        Assert.assertTrue(dist.centroids().size() < 7 * sizeGuide);
+        System.out.printf("dist\tx\tQ\terror\n");
+        for (int i = 0; i < xValues.length; i++) {
+            double x = xValues[i];
+            double q = qValues[i];
+            double estimate = dist.cdf(x);
+            System.out.printf("%s\t%.3g\t%.3f\t%.6f\n", tag, x, q, estimate - q);
         }
     }
+
 
     @Before
     public void setUp() {
