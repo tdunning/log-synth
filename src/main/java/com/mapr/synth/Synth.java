8e6c3f1dc696427022754b265950cdd22a525595
Ted Dunning <tdunning@apache.org>
2014-09-17 22:28:47 -0700

    Improve logging, upgrade guava (for no strong reason)

--- src/main/java/com/mapr/synth/Synth.java
+++ src/main/java/com/mapr/synth/Synth.java
@@ -13,13 +13,18 @@ import org.kohsuke.args4j.spi.IntOptionHandler;
 import org.kohsuke.args4j.spi.Setter;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.PrintStream;
+import java.lang.management.ManagementFactory;
+import java.lang.management.ThreadMXBean;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardOpenOption;
+import java.security.AccessControlException;
 import java.util.List;
 import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicLong;
 
 /**
@@ -44,8 +49,8 @@ public class Synth {
                     "[-output output-directory-name] ");
             throw e;
         }
-        Preconditions.checkArgument(opts.threads > 0 && opts.threads <= 200,
-                "Must have at least one thread and no more than 200");
+        Preconditions.checkArgument(opts.threads > 0 && opts.threads <= 2000,
+                "Must have at least one thread and no more than 2000");
 
         if (opts.threads > 1) {
             Preconditions.checkArgument(!"-".equals(opts.output),
@@ -60,48 +65,40 @@ public class Synth {
                 String.format("Couldn't create directory %s", opts.output));
 
 
-        final SchemaSampler s = new SchemaSampler(opts.schema);
+        final SchemaSampler sampler = new SchemaSampler(opts.schema);
         final AtomicLong rowCount = new AtomicLong();
-        final double t0 = System.nanoTime()*1e-9;
 
-        List<Callable<Integer>> tasks = Lists.newArrayList();
+        final List<ReportingWorker> tasks = Lists.newArrayList();
         int limit = (opts.count + opts.threads - 1) / opts.threads;
         int remaining = opts.count;
         for (int i = 0; i < opts.threads; i++) {
 
             final int count = Math.min(limit, remaining);
-            final int fileNumber = i;
             remaining -= count;
 
-            tasks.add(new Callable<Integer>() {
-                int localCount = count;
-
-                @Override
-                public Integer call() throws Exception {
-                    if ("-".equals(opts.output)) {
-                        return generateFile(opts, s, System.out, localCount, rowCount);
-                    } else {
-                        Path outputPath = new File(opts.output, String.format("synth-%04d", fileNumber)).toPath();
-                        try (PrintStream out = new PrintStream(Files.newOutputStream(outputPath,
-                                StandardOpenOption.CREATE, StandardOpenOption.WRITE, StandardOpenOption.TRUNCATE_EXISTING))) {
-                            return generateFile(opts, s, out, localCount, rowCount);
-                        }
-                    }
-                }
-            });
+            tasks.add(new ReportingWorker(opts, sampler, rowCount, count, i));
         }
 
+        final double t0 = System.nanoTime() * 1e-9;
         ExecutorService pool = Executors.newFixedThreadPool(opts.threads);
         ScheduledExecutorService blinker = Executors.newScheduledThreadPool(1);
+        final AtomicBoolean finalRun = new AtomicBoolean(false);
+
+        final PrintStream sideLog = new PrintStream(new FileOutputStream("side-log"));
         Runnable blink = new Runnable() {
             public double oldT;
             private long oldN;
+
 
             @Override
             public void run() {
                 double t = System.nanoTime() * 1e-9;
                 long n = rowCount.get();
-                System.out.printf("%d\t%.1f\t%d\t%.3f\n", opts.threads, t - t0, n, (n - oldN) / (t - oldT));
+                System.out.printf("%s\t%d\t%.1f\t%d\t%.1f\t%.3f\n", finalRun.get() ? "F" : "R", opts.threads, t - t0, n, n / (t - t0), (n - oldN) / (t - oldT));
+                for (ReportingWorker task : tasks) {
+                    ReportingWorker.ThreadReport r = task.report();
+                    sideLog.printf("\t%d\t%.2f\t%.2f\t%.2f\t%.1f\t%.1f\n", r.fileNumber, r.threadTime, r.userTime, r.wallTime, r.rows / r.threadTime, r.rows / r.wallTime);
+                }
                 oldN = n;
                 oldT = t;
             }
@@ -119,33 +116,140 @@ public class Synth {
                 String.format("Expected to generate %d lines of output, but actually generated %d", opts.count, total));
         pool.shutdownNow();
         blinker.shutdownNow();
+        finalRun.set(true);
+        sideLog.close();
         blink.run();
     }
 
-    private static int generateFile(Options opts, SchemaSampler s, PrintStream out, int count, AtomicLong rowCount) {
-        header(opts.format, s.getFieldNames(), out);
-        for (int i = 0; i < count; i++) {
-            format(opts.format, opts.quote, s.getFieldNames(), s.sample(), out);
-            if (i % REPORTING_DELTA == 0) {
-                rowCount.addAndGet(REPORTING_DELTA);
+    private static class ReportingWorker implements Callable<Integer> {
+        private final Options opts;
+        private final SchemaSampler sampler;
+        private final AtomicLong rowCount;
+        private final int count;
+        private final int fileNumber;
+        int localCount;
+        private ThreadMXBean mx;
+        private AtomicLong wallTime;
+        private AtomicLong threadTime;
+        private AtomicLong userTime;
+        private AtomicLong lastUserTime;
+        final AtomicLong lastWall;
+        final AtomicLong lastThreadTime;
+        final AtomicLong lastRowCount;
+
+        ReportingWorker(final Options opts, final SchemaSampler sampler, final AtomicLong rowCount, final int count, final int fileNumber) {
+            mx = ManagementFactory.getThreadMXBean();
+            try {
+                if (mx.isThreadCpuTimeSupported())
+                    mx.setThreadCpuTimeEnabled(true);
+                else
+                    throw new AccessControlException("");
+            } catch (AccessControlException e) {
+                System.out.println("CPU Usage monitoring is not available!");
+                System.exit(0);
+            }
+
+            this.opts = opts;
+            this.sampler = sampler;
+            this.rowCount = rowCount;
+            this.count = count;
+            this.fileNumber = fileNumber;
+            localCount = this.count;
+            lastWall = new AtomicLong(System.nanoTime());
+            wallTime = new AtomicLong(lastWall.get());
+            lastThreadTime = new AtomicLong(mx.getCurrentThreadCpuTime());
+            threadTime = new AtomicLong(lastThreadTime.get());
+            lastUserTime = new AtomicLong(mx.getCurrentThreadUserTime());
+            userTime = new AtomicLong(lastThreadTime.get());
+            lastRowCount = new AtomicLong(0);
+        }
+
+        @Override
+        public Integer call() throws Exception {
+            if ("-".equals(opts.output)) {
+                return generateFile(opts, sampler, System.out, localCount);
+            } else {
+                Path outputPath = new File(opts.output, String.format("synth-%04d", fileNumber)).toPath();
+
+                try (PrintStream out = new PrintStream(Files.newOutputStream(outputPath,
+                        StandardOpenOption.CREATE, StandardOpenOption.WRITE, StandardOpenOption.TRUNCATE_EXISTING))) {
+
+                    header(opts.format, sampler.getFieldNames(), out);
+                    int rows = 0;
+                    while (rows < localCount) {
+                        int k = Math.min(localCount - rows, REPORTING_DELTA);
+                        rows += k;
+                        rowCount.addAndGet(generateFile(opts, sampler, out, k));
+                        wallTime.set(System.nanoTime());
+                        threadTime.set(mx.getCurrentThreadCpuTime());
+                        userTime.set(mx.getCurrentThreadUserTime());
+                    }
+
+                    return rows;
+                }
             }
         }
-        return count;
+
+        public static void header(Format format, List<String> names, PrintStream out) {
+            switch (format) {
+                case TSV:
+                    out.printf("%s\n", withTabs.join(names));
+                    break;
+                case CSV:
+                    out.printf("%s\n", withCommas.join(names));
+                    break;
+            }
+        }
+
+        public static int generateFile(Options opts, SchemaSampler s, PrintStream out, int count) {
+            for (int i = 0; i < count; i++) {
+                format(opts.format, opts.quote, s.getFieldNames(), s.sample(), out);
+            }
+            return count;
+        }
+
+        public ThreadReport report() {
+            return new ThreadReport();
+        }
+
+        public class ThreadReport {
+            long rows;
+            int fileNumber = ReportingWorker.this.fileNumber;
+            double wallTime;
+            double threadTime;
+            double userTime;
+
+            public ThreadReport() {
+                while (true) {
+                    long oldWall = lastWall.get();
+                    long oldThread = lastThreadTime.get();
+                    long oldUser = lastUserTime.get();
+                    long oldRowCount = lastRowCount.get();
+
+                    long wall = ReportingWorker.this.wallTime.get();
+                    long thread = ReportingWorker.this.threadTime.get();
+                    long user = ReportingWorker.this.userTime.get();
+                    long rowCount = ReportingWorker.this.rowCount.get();
+
+                    wallTime = (wall - oldWall) * 1e-9;
+                    threadTime = (thread - oldThread) * 1e-9;
+                    userTime = (user - oldUser) * 1e-9;
+                    rows = rowCount - oldRowCount;
+
+                    if (lastWall.compareAndSet(oldWall, wall) && lastThreadTime.compareAndSet(oldThread, thread)
+                            && lastRowCount.compareAndSet(oldRowCount, rowCount)) {
+                        // nobody stomped on our report
+                        return;
+                    }
+                }
+            }
+        }
     }
+
 
     static Joiner withCommas = Joiner.on(",");
     static Joiner withTabs = Joiner.on("\t");
 
-    private static void header(Format format, List<String> names, PrintStream out) {
-        switch (format) {
-            case TSV:
-                out.printf("%s\n", withTabs.join(names));
-                break;
-            case CSV:
-                out.printf("%s\n", withCommas.join(names));
-                break;
-        }
-    }
 
     private static void format(Format format, Quote quoteConvention, List<String> names, JsonNode fields, PrintStream out) {
         switch (format) {
