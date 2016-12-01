4d82ec9a4fe31343ab14d39a51a292728318a0ce
Ted Dunning <tdunning@apache.org>
2016-12-01 10:47:04 +0900

    Cleaned up car sim

--- src/main/java/com/mapr/synth/samplers/CarOnRoadSampler.java
+++ src/main/java/com/mapr/synth/samplers/CarOnRoadSampler.java
@@ -24,14 +24,13 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.JsonNodeFactory;
-import com.google.common.collect.ImmutableMap;
+import com.google.common.base.Objects;
 import com.google.common.collect.Lists;
+import com.mapr.synth.UnitParser;
 
 import java.util.Collections;
 import java.util.List;
-import java.util.Map;
 import java.util.Random;
-import java.util.concurrent.TimeUnit;
 import java.util.function.BiConsumer;
 import java.util.function.Function;
 import java.util.regex.Matcher;
@@ -47,66 +46,133 @@ import java.util.regex.Pattern;
  * for parts of the road at certain times.
  */
 public class CarOnRoadSampler extends FieldSampler {
-    private static ObjectMapper mapper = new ObjectMapper();
+    private static final ObjectMapper mapper = new ObjectMapper();
 
-    private final Pattern ratePattern = Pattern.compile("([0-9.e\\-]+)\\s*(/(([smhd])|sec|second|m|min|minute|h|hour|d|day))?");
-    private final Map<String, Double> rateMap =
-            ImmutableMap.<String, Double>builder()
-                    .put("/s", 1.0 / TimeUnit.SECONDS.toSeconds(1L))
-                    .put("/sec", 1.0 / TimeUnit.SECONDS.toSeconds(1L))
-                    .put("/second", 1.0 / TimeUnit.SECONDS.toSeconds(1L))
-                    .put("/m", 1.0 / TimeUnit.MINUTES.toSeconds(1L))
-                    .put("/min", 1.0 / TimeUnit.MINUTES.toSeconds(1L))
-                    .put("/minute", 1.0 / TimeUnit.MINUTES.toSeconds(1L))
-                    .put("/h", 1.0 / TimeUnit.HOURS.toSeconds(1L))
-                    .put("/hour", 1.0 / TimeUnit.HOURS.toSeconds(1L))
-                    .put("/d", 1.0 / TimeUnit.DAYS.toSeconds(1L))
-                    .put("/day", 1.0 / TimeUnit.DAYS.toSeconds(1L))
-                    .build();
+    private static final JsonNodeFactory nodeFactory = JsonNodeFactory.withExactBigDecimals(false);
 
-    private final Pattern timePattern = Pattern.compile("([0-9.e\\-]+)\\s*([smhd])?");
-    private final Map<String, Double> timeUnitMap = ImmutableMap.of(
-            "s", (double) TimeUnit.SECONDS.toSeconds(1L),
-            "m", (double) TimeUnit.MINUTES.toSeconds(1L),
-            "h", (double) TimeUnit.HOURS.toSeconds(1L),
-            "d", (double) TimeUnit.DAYS.toSeconds(1L));
+    private double meanInterval = 0;                                // in seconds/car
+    private double averageSpeed;                                    // in m/s
+    private double sdSpeed;                                         // speed variation
 
-    private static final double INCH = 2.54e-2;
-    private static final double FOOT = 12 * INCH;
-    private static final double MILE = 5280 * FOOT;
-    private static final double HOUR = 3600.0;
+    private final List<DistanceEvent> events = Lists.newArrayList();      // all sensors and slowdowns go here
 
-    private final Pattern speedPattern = Pattern.compile("([0-9.e\\-]+)\\s*((mph)|(kph)|(m/s))?");
+    private final Random rand = new Random();
 
-    private final Map<String, Double> speedMap = ImmutableMap.of(
-            "mph", MILE / HOUR,
-            "kph", 1000 / HOUR,
-            "m/s", 1.0);
+    private double currentTime;
 
-    private final Pattern distancePattern = Pattern.compile("([0-9.e\\-]+)\\s*((m)|(km)|(mile))?");
+    @SuppressWarnings("UnusedDeclaration")
+    public void setArrival(String rate) {
+        this.meanInterval = 1.0 / UnitParser.parseRate(rate);
+    }
 
-    private final Map<String, Double> distanceMap = ImmutableMap.of(
-            "km", 1000.0,
-            "mile", MILE,
-            "m", 1.0);
+    @SuppressWarnings("UnusedDeclaration")
+    public void setSpeed(String speed) {
+        this.averageSpeed = UnitParser.parseSpeed(speed);
+    }
 
-    private JsonNodeFactory nodeFactory = JsonNodeFactory.withExactBigDecimals(false);
+    @SuppressWarnings("UnusedDeclaration")
+    public void setVariance(String speed) {
+        this.sdSpeed = UnitParser.parseSpeed(speed);
+    }
 
-    private static abstract class DistanceEvent {
-        abstract double getLocation();
+    @SuppressWarnings("UnusedDeclaration")
+    public void setSensors(JsonNode sensors) {
+        double sensorUnit = UnitParser.parseDistanceUnit(sensors.get("unit").asText());
 
-        double adjustSpeed(double time, double speed) {
+        JsonNode positions = sensors.get("locations");
+        if (positions == null) {
+            positions = sensors.get("positions");
+        }
+        if (positions == null) {
+            throw new IllegalArgumentException("Needed \"locations\" parameter for sensors definition");
+        }
+        for (JsonNode position : positions) {
+            String id = String.format("s-%04x-%02x-%06x", rand.nextInt(), rand.nextInt(), rand.nextInt());
+            events.add(new Sensor(id, position.asDouble() * sensorUnit));
+        }
+        Collections.sort(events);
+    }
+
+    @SuppressWarnings("UnusedDeclaration")
+    public void setSlowdown(ArrayNode slowdowns) throws JsonProcessingException {
+        for (JsonNode slowdown : slowdowns) {
+            SlowDown jam = mapper.treeToValue(slowdown, SlowDown.class);
+            events.add(jam);
+            events.add(jam.releaseEvent());
+        }
+        Collections.sort(events);
+    }
+
+    @Override
+    public JsonNode sample() {
+        currentTime += -Math.log(rand.nextDouble()) * meanInterval;
+
+        double nominalSpeed = averageSpeed + rand.nextGaussian() * sdSpeed;
+        double speed = nominalSpeed;
+
+        ArrayNode sensorMeasurements = nodeFactory.arrayNode();
+        double t = currentTime;
+        double location = 0;
+        for (DistanceEvent event : events) {
+            t += (event.getLocation() - location) / speed;
+            location = event.getLocation();
+            speed = event.adjustSpeed(t, speed, nominalSpeed);
+            TrafficSensorEvent r = event.getMeasurement(t, speed);
+            if (r != null) {
+                JsonNode measurementAsJson = mapper.valueToTree(r);
+                sensorMeasurements.add(measurementAsJson);
+            }
+        }
+        return sensorMeasurements;
+    }
+
+    public static class TrafficSensorEvent {
+        private final String id;
+        private final double time;
+        private final double speed;
+
+        public TrafficSensorEvent(String id, double time, double speed) {
+            this.id = id;
+            this.time = time;
+            this.speed = speed;
+        }
+
+        @SuppressWarnings("UnusedDeclaration")
+        public String getId() {
+            return id;
+        }
+
+        @SuppressWarnings("UnusedDeclaration")
+        public double getSpeed() {
             return speed;
         }
 
-        TrafficSensorEvent getMeasurement(double time, double speed) {
+        @SuppressWarnings("UnusedDeclaration")
+        public double getTime() {
+            return time;
+        }
+    }
+
+    public abstract static class DistanceEvent implements Comparable<DistanceEvent> {
+        abstract double getLocation();
+
+        public double adjustSpeed(double time, double speed, double originalSpeed) {
+            return speed;
+        }
+
+        public TrafficSensorEvent getMeasurement(double time, double speed) {
             return null;
+        }
+
+        @Override
+        public int compareTo(DistanceEvent other) {
+            return Double.compare(getLocation(), other.getLocation());
         }
     }
 
     private static class Sensor extends DistanceEvent {
-        String id;
-        double location;
+        final String id;
+        final double location;
 
         public Sensor(String uuid, double position) {
             id = uuid;
@@ -124,7 +190,7 @@ public class CarOnRoadSampler extends FieldSampler {
         }
     }
 
-    class SlowDown extends DistanceEvent {
+    public static class SlowDown extends DistanceEvent {
         double speed;
         double startPoint;
         double endPoint;
@@ -140,7 +206,7 @@ public class CarOnRoadSampler extends FieldSampler {
         }
 
         @Override
-        double adjustSpeed(double time, double speed) {
+        public double adjustSpeed(double time, double speed, double originalSpeed) {
             if (time >= startTime && time < endTime) {
                 return this.speed;
             } else {
@@ -148,22 +214,25 @@ public class CarOnRoadSampler extends FieldSampler {
             }
         }
 
+        @SuppressWarnings("UnusedDeclaration")
         public void setSpeed(String speed) {
-            this.speed = parseSpeed(speed);
+            this.speed = UnitParser.parseSpeed(speed);
         }
 
         final Pattern hyphenated = Pattern.compile("(.*)\\s*-\\s*(.*)");
 
+        @SuppressWarnings("UnusedDeclaration")
         public void setLocation(String location) {
-            parsePair(location, CarOnRoadSampler.this::parseDistance,
+            parsePair(location, UnitParser::parseDistance,
                     (Double start, Double end) -> {
                         this.startPoint = start;
                         this.endPoint = end;
                     });
         }
 
+        @SuppressWarnings("UnusedDeclaration")
         public void setTime(String time) {
-            parsePair(time, CarOnRoadSampler.this::parseTime,
+            parsePair(time, UnitParser::parseTime,
                     (Double start, Double end) -> {
                         this.startTime = start;
                         this.endTime = end;
@@ -178,131 +247,50 @@ public class CarOnRoadSampler extends FieldSampler {
                 throw new IllegalArgumentException(String.format("Bad location: %s", location));
             }
         }
-    }
-
-    double meanInterval = 0;                                // in seconds/car
-    double averageSpeed;                                    // in m/s
-    double sdSpeed;                                         // speed variation
-
-    List<DistanceEvent> events = Lists.newArrayList();      // all sensors and slowdowns go here
-
-    double sensorUnit;                                      // what is the sensor distance unit
-
-    Random rand = new Random();
-
-    double currentTime;
-
-    @SuppressWarnings("UnusedDeclaration")
-    public void setArrival(String rate) {
-        this.meanInterval = 1.0 / parseRate(rate);
-    }
-
-    public double parseSpeed(String speed) {
-        return unitParse(speed, speedPattern, speedMap, "Invalid speed argument: %s");
-    }
-
-    public double parseRate(String rate) {
-        return unitParse(rate, ratePattern, rateMap, "Invalid rate argument: %s");
-    }
-
-    public double parseDistance(String distance) {
-        return unitParse(distance, distancePattern, distanceMap, "Invalid location argument: %s");
-    }
-
-    private double parseTime(String s) {
-        return unitParse(s, timePattern, timeUnitMap, "Bad time expression: %s");
-    }
-
-    private double unitParse(String value, Pattern pattern, Map<String, Double> translation, String errorFormat) {
-        Matcher m = pattern.matcher(value);
-        if (m.matches()) {
-            // group(1) is the number, group(2) is either empty (default to /s) or /d or some such.
-            double unit = (m.groupCount() > 1) ? translation.get(m.group(2)) : 1.0;
-            double raw = Double.parseDouble(m.group(1));
-            return raw * unit;
-        } else {
-            throw new IllegalArgumentException(String.format(errorFormat, value));
-        }
-    }
-
-    @SuppressWarnings("UnusedDeclaration")
-    public void setSpeed(String speed) {
-        this.averageSpeed = parseSpeed(speed);
-    }
-
-    @SuppressWarnings("UnusedDeclaration")
-    public void setVariance(String speed) {
-        this.sdSpeed = parseSpeed(speed);
-    }
-
-    @SuppressWarnings("UnusedDeclaration")
-    public void setSensors(JsonNode sensors) {
-        String unit = sensors.get("unit").asText();
-        double sensorUnit = distanceMap.containsKey(unit) ? distanceMap.get(unit) : 1;
-
-        JsonNode positions = sensors.get("locations");
-        if (positions == null) {
-            positions = sensors.get("positions");
-        }
-        if (positions == null) {
-            throw new IllegalArgumentException("Needed \"locations\" parameter for sensors definition");
-        }
-        for (JsonNode position : positions) {
-            String id = String.format("s-%04x-%02x-%06x", rand.nextInt(), rand.nextInt(), rand.nextInt());
-            events.add(new Sensor(id, position.asDouble() * sensorUnit));
-        }
-        Collections.sort(events, (e1, e2) -> Double.compare(e1.getLocation(), e2.getLocation()));
-    }
 
 
-    public void setSlowdown(ArrayNode slowdowns) throws JsonProcessingException {
-        for (JsonNode slowdown : slowdowns) {
-            events.add(mapper.treeToValue(slowdown, SlowDown.class));
-        }
-        Collections.sort(events, (e1, e2) -> Double.compare(e1.getLocation(), e2.getLocation()));
-    }
-
-    private static class TrafficSensorEvent {
-        private String id;
-        private double time;
-        private double speed;
-
-        public TrafficSensorEvent(String id, double time, double speed) {
-            this.id = id;
-            this.time = time;
-            this.speed = speed;
-        }
-
-        public String getId() {
-            return id;
-        }
-
+        @SuppressWarnings("UnusedDeclaration")
         public double getSpeed() {
             return speed;
         }
 
-        public double getTime() {
-            return time;
+        public DistanceEvent releaseEvent() {
+            return new Speedup(endPoint);
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            SlowDown slowDown = (SlowDown) o;
+            return Objects.equal(speed, slowDown.speed) &&
+                    Objects.equal(startPoint, slowDown.startPoint) &&
+                    Objects.equal(endPoint, slowDown.endPoint) &&
+                    Objects.equal(startTime, slowDown.startTime) &&
+                    Objects.equal(endTime, slowDown.endTime);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hashCode(speed, startPoint, endPoint, startTime, endTime);
         }
     }
 
-    @Override
-    public JsonNode sample() {
-        currentTime += -Math.log(rand.nextDouble()) * meanInterval;
+    public static class Speedup extends  DistanceEvent {
+        private final double location;
 
-        double speed = averageSpeed + rand.nextGaussian() * sdSpeed;
-
-        ArrayNode sensorMeasurments = nodeFactory.arrayNode();
-        double start = currentTime;
-        for (DistanceEvent event : events) {
-            double t = start + event.getLocation() / speed;
-            speed = event.adjustSpeed(t, speed);
-            TrafficSensorEvent r = event.getMeasurement(t, speed);
-            if (r != null) {
-                JsonNode measurementAsJson = mapper.valueToTree(r);
-                sensorMeasurments.add(measurementAsJson);
-            }
+        public Speedup(double location) {
+            this.location = location;
         }
-        return sensorMeasurments;
+
+        @Override
+        double getLocation() {
+            return location;
+        }
+
+        @Override
+        public double adjustSpeed(double time, double speed, double originalSpeed) {
+            return originalSpeed;
+        }
     }
 }
--- src/test/java/com/mapr/synth/samplers/CarTest.java
+++ src/test/java/com/mapr/synth/samplers/CarTest.java
@@ -19,14 +19,18 @@
 
 package com.mapr.synth.samplers;
 
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.base.Charsets;
 import com.google.common.io.Resources;
 import org.junit.Test;
 
 import java.io.IOException;
+import java.util.List;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class CarTest {
     @Test
@@ -58,14 +62,89 @@ public class CarTest {
     @Test
     public void testArrivalTimes() throws IOException {
         SchemaSampler s = new SchemaSampler(Resources.toString(Resources.getResource("schema032.json"), Charsets.UTF_8));
-
-
+        double meanDelay = 0;
+        double exponentialMean = 0;
+        double sd = 0;
+        double previous = 0;
+        for (int i = 0; i < 10000; i++) {
+            // time to first sensor should show steady increase
+            JsonNode x = s.sample();
+            double t = x.get("delayed").get(0).get("time").asDouble();
+            double delay = t - previous;
+            double expt = Math.exp(-delay / 60);
+            assertTrue(expt >= 0);
+            assertTrue(expt <= 1);
+            exponentialMean += expt;
+            sd += (expt - 0.5) * (expt - 0.5);
+            previous = t;
+            meanDelay += delay;
+        }
+        meanDelay /= 10000;
+        exponentialMean /= 10000;
+        sd /= 10000;
+        // exponential distribution should have correct mean
+        assertEquals(60, meanDelay, 3);
+        // and exp(delay) should be uniformly distributed
+        assertEquals(0.5, exponentialMean, 0.02);
+        assertEquals(1 / 12.0, sd, 0.002);
     }
 
     @Test
     public void testSlowdown() throws IOException {
         SchemaSampler s = new SchemaSampler(Resources.toString(Resources.getResource("schema033.json"), Charsets.UTF_8));
 
+        Histogram[] histograms = new Histogram[7];
+        for (int j = 0; j < 6; j++) {
+            histograms[j] = new Histogram();
+        }
 
+        for (int i = 0; i < 500; i++) {
+            // times between sensors should be tightly clustered
+            // outside of the traffic jam, the clustering is all around a single value
+            // in the traffic jam area, there should be two values.
+            JsonNode x = s.sample();
+
+            for (int j = 0; j < 6; j++) {
+                double t0 = x.get("traffic").get(j).get("time").asDouble();
+                double t1 = x.get("traffic").get(j + 1).get("time").asDouble();
+                histograms[j].add(t1 - t0);
+            }
+        }
+        assertEquals("Times from 1 to 2 km", 500, histograms[0].counts[60]);
+        assertEquals("Times from 2 to 3 km", 500, histograms[1].counts[60]);
+        assertEquals("Times from 3 to 4 km", 450.0, histograms[2].counts[60], 20);
+        assertEquals("Times from 3 to 4 km", 50.0, histograms[2].counts[120], 20);
+        assertEquals("Times from 4 to 5 km", 450.0, histograms[3].counts[60], 20);
+        assertEquals("Times from 4 to 5 km", 50.0, histograms[3].counts[120], 20);
+        assertEquals("Times from 5 to 6 km", 500, histograms[4].counts[60]);
+        assertEquals("Times from 6 to 7 km", 500, histograms[5].counts[60]);
+    }
+
+    private class Histogram {
+        int[] counts = new int[180];
+
+        void add(double x) {
+            int bin = (int) Math.rint(x);
+            if (bin < 0) {
+                counts[0]++;
+            } else if (bin >= counts.length) {
+                counts[counts.length - 1]++;
+            } else {
+                counts[bin]++;
+            }
+        }
+    }
+
+    @Test
+    public void testSlowdownConstructor() throws Exception {
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode j1 = mapper.readTree("{\"speed\":\"30 kph\", \"location\":\"3 km - 5 km\", \"time\": \"5min - 10min\"}");
+        CarOnRoadSampler.SlowDown s1 = mapper.convertValue(j1, CarOnRoadSampler.SlowDown.class);
+        assertEquals(30_000.0 / 3600, s1.getSpeed(), 1e-11);
+
+        JsonNode j2 = mapper.readTree("[{\"speed\":\"30 kph\", \"location\":\"3 km - 5 km\", \"time\": \"5min - 10min\"}]");
+        List<CarOnRoadSampler.SlowDown> s2 = mapper.convertValue(j2, new TypeReference<List<CarOnRoadSampler.SlowDown>>() {
+        });
+        assertEquals(s1, s2.get(0));
     }
 }
