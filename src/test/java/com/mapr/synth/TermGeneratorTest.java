1088028036bf82b296db0d95790dcf00936b69fe
Ted Dunning <ted.dunning@gmail.com>
2022-10-18 23:00:04 -0700

    Removed distinctVocabularies because distribution test didn't make sense. Also limited verbose test outputs

--- src/test/java/com/mapr/synth/TermGeneratorTest.java
+++ src/test/java/com/mapr/synth/TermGeneratorTest.java
@@ -73,42 +73,6 @@ public class TermGeneratorTest {
     }
 
     @Test
-    public void distinctVocabularies() {
-        TermGenerator x1 = new TermGenerator(WORDS, 10, 0.2);
-        final Multiset<String> k1 = HashMultiset.create();
-        for (int i = 0; i < 50000; i++) {
-            k1.add(x1.sample());
-        }
-
-        TermGenerator x2 = new TermGenerator(WORDS, 10, 0.2);
-        final Multiset<String> k2 = HashMultiset.create();
-        for (int i = 0; i < 50000; i++) {
-            k2.add(x2.sample());
-        }
-
-        final NormalDistribution normal = new NormalDistribution();
-
-        List<Double> scores =
-                Sets.union(k1.elementSet(), k2.elementSet()).stream()
-                        .map(s -> normal.cumulativeProbability(
-                                LogLikelihood.rootLogLikelihoodRatio(
-                                        k1.count(s), 50000 - k1.count(s),
-                                        k2.count(s), 50000 - k2.count(s))))
-                        .sorted(Ordering.natural())
-                        .collect(Collectors.toList());
-        int n = scores.size();
-//        System.out.printf("%.5f, %.5f, %.5f, %.5f, %.5f, %.5f, %.5f", scores.get(0), scores.get((int) (0.05*n)), scores.get(n / 4), scores.get(n / 2), scores.get(3 * n / 4), scores.get((int) (0.95 * n)), scores.get(n - 1));
-        int i = 0;
-        for (Double score : scores) {
-            if (i % 10 == 0) {
-                System.out.printf("%.6f\t%.6f\n", (double) i / n, score);
-            }
-
-            i++;
-        }
-    }
-
-    @Test
     public void speciesCounts() {
         final boolean transpose = false;
 
@@ -138,17 +102,17 @@ public class TermGeneratorTest {
             r.add(counts);
         }
 
-        System.out.printf("%d\n", vocabulary.size());
+//        System.out.printf("%d\n", vocabulary.size());
         for (Multiset<Integer> day : r) {
             vocabulary.addAll(day.elementSet());
-            String sep = "";
-            System.out.printf("%s%s", sep, vocabulary.size());
-            sep = "\t";
-            for (Integer s : vocabulary) {
-                System.out.printf("%s%s", sep, day.count(s));
-                sep = "\t";
-            }
-            System.out.print("\n");
+//            String sep = "";
+//            System.out.printf("%s%s", sep, vocabulary.size());
+//            sep = "\t";
+//            for (Integer s : vocabulary) {
+//                System.out.printf("%s%s", sep, day.count(s));
+//                sep = "\t";
+//            }
+//            System.out.print("\n");
         }
 
         Multiset<Integer> total = HashMultiset.create();
@@ -157,13 +121,13 @@ public class TermGeneratorTest {
                 total.add(species, day.count(species));
             }
         }
-        String sep = "";
-        System.out.printf("%s%s", sep, total.elementSet().size());
-        sep = "\t";
-        for (Integer s : vocabulary) {
-            System.out.printf("%s%s", sep, total.count(s));
-            sep = "\t";
-        }
-        System.out.print("\n");
+//        String sep = "";
+//        System.out.printf("%s%s", sep, total.elementSet().size());
+//        sep = "\t";
+//        for (Integer s : vocabulary) {
+//            System.out.printf("%s%s", sep, total.count(s));
+//            sep = "\t";
+//        }
+//        System.out.print("\n");
     }
 }
